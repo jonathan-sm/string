d91e7369e42fb4cc322a8cc91a481cdb26cf6128
home <jonathan_sm@mail.ru>
2023-10-09 02:27:38 +0300

    Initial commit

---
+++ string/src/main/java/com/skypro/string/string/BasketService.java
@@ -0,0 +1,23 @@
+package com.skypro.string.string;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+@Service
+public class BasketService {
+    private final Basket basket;
+
+    public BasketService(Basket basket) {
+        this.basket = basket;
+    }
+
+    public List<Integer> add(List<Integer> ids) {
+        return basket.add(ids);
+    }
+
+    public Map<Integer, Integer> get() {
+        return basket.get();
+    }
+}
+
